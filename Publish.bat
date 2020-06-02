5821ca70c85fab961dc98349c2e651f166836f90
alexinea <forerunner@outlook.com>
2020-06-02 14:54:16 +0800

    Update solution files

--- Publish.bat
+++ Publish.bat
@@ -10,6 +10,8 @@ for /R "nuget_pub" %%s in (*) do (
     del %%s
 )
 
+set /p key=input key:
+
 dotnet pack src/Cosmos.Extensions.Dependency.Core -c Release -o nuget_pub
 dotnet pack src/Cosmos.Extensions.Autofac -c Release -o nuget_pub
 dotnet pack src/Cosmos.Extensions.AspectCoreInjector -c Release -o nuget_pub
@@ -22,7 +24,6 @@ for /R "nuget_pub" %%s in (*symbols.nupkg) do (
 echo.
 echo.
 
-set /p key=input key:
 set source=https://api.nuget.org/v3/index.json
 
 for /R "nuget_pub" %%s in (*.nupkg) do ( 
